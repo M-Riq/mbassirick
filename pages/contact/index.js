0ec0e0c6294e267699f08964b861c7f320cc5d36
rickHateCode <hanonyme99@gmail.com>
2025-09-10 20:40:47 +0100

    Fix: ajout de la key manquante pour les éléments dans la fait

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -19,9 +19,9 @@ const Contact = () => {
           {/* form */}
           <motion.form variants = {fadeIn('down', 0.2)} initial = "hidden" animate = "show" exit ="hidden" className='flex-1 flex flex-col gap-6 w-full mx-auto'>
             {/* input group */}
-            <div className='flex gap-x-6 w-full'>
-              <input type = 'text' placeholder = 'name' className='input'/>
-              <input type = 'text' placeholder = 'email' className='input'/>
+            <div className="flex gap-x-6 w-full">
+              <input type = "text" placeholder = "name" className="input"/>
+              <input type = "text" placeholder = "email" className="input"/>
             </div>
             <input type='text' placeholder='subject' className='input'/>
             <textarea placeholder='message' className='textarea'></textarea>
